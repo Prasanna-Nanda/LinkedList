1072b720adc6236c780ed3c3a386a69f43b3c61b
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-03-05 22:23:11 +0530

    Added Library

---
+++ Library.java
@@ -0,0 +1,31 @@
+package Library;
+
+import MyNodeTest.MyNode;
+import org.junit.Test;
+
+public class MyLinkedList {
+    public MyNode<Integer> head;
+    public MyNode<Integer> tail;
+
+    @Test
+    public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+    
+    
+    
+    public void append(MyNode<Integer> mySecondNode) {
+    }
+
+    public void popLast() {
+    }
+
+    public void printMyNodes() {
+    }
+
+    public void pop() {
+    }
+
+    public void insert(MyNode<Integer> myFirstNode, MyNode<Integer> mySecondNode) {
+
