cac0b20df6f0c9d169d547b66c712f4cdaeb2dbe
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-03-05 21:44:08 +0530

    Added CreationOfLinkedList

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -18,3 +18,19 @@ public class MyLinkedListTest {
         assertTrue(result);
     }
 
+    @Test
+    public void givenThreeNumbersShouldPassedLinkedListTestToTop() {
+        MyNode<Integer> myFirstNode=new MyNode<>(70);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(56);
+        MyLinkedList myLinkedList=new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result=myLinkedList.head.equals(myThirdNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myFirstNode);
+        assertTrue(result);
+    }
+}
