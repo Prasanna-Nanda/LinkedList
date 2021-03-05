230af6270ef3d5d88d10ace57e3fcaf100662834
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-03-05 22:08:59 +0530

    Added DeleteLastElement

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -83,4 +83,20 @@ public class MyLinkedListTest {
         assertTrue(result);
     }
 
+     @Test
+    public void  givenThreeNumbersDeleteLastShouldPassed(){
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.popLast();
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.tail.equals(mySecondNode);
+        assertTrue(result);
+    }
+
 }
